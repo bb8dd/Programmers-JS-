b16d9ee0f0297bf8621ba1c3bc08e65b79e64caa
bb8dd <ldh19971027@gmail.com>
2022-07-06 23:29:59 +0900

    update

--- app.js
+++ app.js
@@ -880,7 +880,7 @@ function solution(arr1, arr2) {
 // 놀이기구를 count번 타게 되면 
 // 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 
 // solution 함수를 완성하세요.
-// 단, 금액이 부족하지 않으면 0을 return 하세요.
+// 단, 금액이 부족하지 않으면 0을 return 하세요
 
 // 제한사항
 // 놀이기구의 이용료 price : 1 ≤ price ≤ 2,500, price는 자연수
