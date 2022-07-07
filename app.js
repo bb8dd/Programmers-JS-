621876b9f8236f42b861a7d95075cc1c4d0e0a6b
ldH <ldh19971027@gmail.com>
2022-07-07 13:50:19 +0900

    ts

--- app.js
+++ app.js
@@ -1260,17 +1260,19 @@ function solution(s) {
 // 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
 
 function solution(participant, completion) {
-   const person = {
-      participant : participant.join().split(),
-      completion : completion.join()
-   }
+   participant = participant.sort();
+   completion = completion.sort();
 
-   completion.forEach((x) => {
-      participant[participant.indexOf(x)] = ""
-   });
-   console.log(participant)
-   participant.join("")
-   return  participant.join("")
+   for(let i = 0; i < participant.length; i++){
+      for(let j = 0; j < completion.length; i++){
+         if(participant[i] === completion[j]) {
+            participant[i] = ""
+            completion[j] =""
+      
+         }   
+      }
+   }
+   return console.log(participant, completion)
 }
 
 //* level 2 
--- greeter.js
+++ greeter.js
@@ -2,3 +2,9 @@ function greeter(_a) {
     var a = _a.a, b = _a.b;
     return a * b;
 }
+
+class Users {
+    constructor(name) {
+        this.name = name
+    }
+} 
